58d7fd7dc6083d039334bb5708629d3d35ab6845
mraafayv <raafayvohra.mrv@gmail.com>
2022-12-09 11:48:41 +0500

    added a dark/light mode toggle button

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,23 +1,37 @@
 import { useTheme } from "../hooks/useTheme";
 
+import modeIcon from '../assets/mode-icon.svg'
 //styles
 import "./ThemeSelector.css";
 
-const themeColors = ['#58249c', '#249c6b', '#b70233'];
+const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 export default function ThemeSelector() {
+  const { changeColor, changeMode, mode } = useTheme();
 
-    const { changeColor } = useTheme()
+
+const toggleMode = () => {
+
+    changeMode(mode === 'dark' ? 'light' : 'dark')
+}
 
   return (
     <div className="theme-selector">
-      <div className="theme-buttons">
-        {themeColors.map(color => (
-            <div 
-                key={color}
-                onClick={() => changeColor(color)}
-                style = {{ background: color }}
+      <div className="mode-selector">
+        <img 
+            src={modeIcon}
+            alt="toggle button for changing mode" 
+            onClick={toggleMode}
+            style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
             />
+      </div>
+      <div className="theme-buttons">
+        {themeColors.map((color) => (
+          <div
+            key={color}
+            onClick={() => changeColor(color)}
+            style={{ background: color }}
+          />
         ))}
       </div>
     </div>
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -6,6 +6,10 @@ const themeReducer = (state, action) => {
     switch(action.type){
         case 'CHANGE_COLOR':
             return {...state, color: action.payload}
+
+        case 'CHANGE_MODE' :
+            return {...state, mode: action.payload}
+            
         default:
             return state;
     }
@@ -15,7 +19,8 @@ const themeReducer = (state, action) => {
 export function ThemeProvider({ children }){
 
    const [state, dispatch] = useReducer(themeReducer, {
-        color: '#58249c'
+        color: '#58249c',
+        mode: 'dark'
     });
 
     const changeColor = (color) => {
@@ -25,8 +30,15 @@ export function ThemeProvider({ children }){
         })
     }
 
+    const changeMode = (mode) => {
+        dispatch({
+            type: "CHANGE_MODE",
+            payload: mode
+        })
+    }
+
     return(
-    <ThemeContext.Provider value={{ ...state, changeColor }}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
         { children }
     </ThemeContext.Provider>
     )
